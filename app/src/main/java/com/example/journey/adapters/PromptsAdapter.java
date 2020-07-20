f75da03de330223e8b9536d5f98cdcd9f9fe8125
Matthew Rene Lacayo <mattlacayo@fb.com>
2020-07-20 14:00:28 -0700

    Created adapter for prompts

---
+++ app/src/main/java/com/example/journey/adapters/PromptsAdapter.java
@@ -0,0 +1,60 @@
+package com.example.journey.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.journey.databinding.ItemPromptBinding;
+import com.example.journey.models.Prompt;
+
+import java.util.List;
+
+public class PromptsAdapter extends RecyclerView.Adapter<PromptsAdapter.ViewHolder> {
+
+    private List<Prompt> prompts;
+    private Context context;
+    private FragmentManager fragmentManager;
+
+    public PromptsAdapter(Context context, FragmentManager fragmentManager, List<Prompt> prompts) {
+        this.context = context;
+        this.fragmentManager = fragmentManager;
+        this.prompts = prompts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(prompts.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return prompts.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private FrameLayout flPromptHolder;
+        private ItemPromptBinding binding;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemPromptBinding.bind(itemView);
+            flPromptHolder = binding.flPromptHolder;
+        }
+
+        private void bind(Prompt prompt) {
+            fragmentManager.beginTransaction().replace(binding.flPromptHolder.getId(), prompt.getResponseFragment()).commit();
+        }
+    }
+}
--- app/src/main/java/com/example/journey/helpers/DBQueryMapper.java
+++ app/src/main/java/com/example/journey/helpers/DBQueryMapper.java
@@ -8,6 +8,7 @@ import com.example.journey.fragments.prompts.CameraAndGalleryPromptFragment;
 import com.example.journey.fragments.prompts.ProudPromptFragment;
 import com.example.journey.fragments.prompts.TravelPromptFragment;
 import com.example.journey.fragments.responses.CameraAndGalleryResponseFragment;
+import com.example.journey.fragments.responses.ProudResponseFragment;
 import com.example.journey.fragments.responses.TravelResponseFragment;
 import com.example.journey.models.Prompt;
 import com.google.android.libraries.places.api.model.Place;
@@ -37,6 +38,8 @@ public class DBQueryMapper {
                 return CameraAndGalleryResponseFragment.newInstance(prompt);
             case Prompt.TRAVEL:
                 return TravelResponseFragment.newInstance(prompt);
+            case Prompt.PROUD:
+                return ProudResponseFragment.newInstance(prompt);
             default:
                 Timber.e("No Fragment Found for that prompt ID");
                 return null;
