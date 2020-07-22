d664e44de813ebf0f3e4a6a28967e09288d22d02
Matthew Rene Lacayo <mattlacayo@fb.com>
2020-07-22 15:11:43 -0700

    Fixed error handling when wifi isn't connected

--- app/src/main/java/com/example/journey/activities/LoginActivity.java
+++ app/src/main/java/com/example/journey/activities/LoginActivity.java
@@ -106,86 +106,91 @@ public class LoginActivity extends AppCompatActivity {
                             goToMainActivity();
                         } else {
                             // If sign in fails, display a message to the user.
-                            String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+                            Exception e = task.getException();
+                            if (task.getException() instanceof FirebaseAuthException) {
+                                String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
 
-                            switch (errorCode) {
+                                switch (errorCode) {
 
-                                case "ERROR_INVALID_CUSTOM_TOKEN":
-                                    Toast.makeText(LoginActivity.this, "The custom token format is incorrect. Please check the documentation.", Toast.LENGTH_LONG).show();
-                                    break;
+                                    case "ERROR_INVALID_CUSTOM_TOKEN":
+                                        Toast.makeText(LoginActivity.this, "The custom token format is incorrect. Please check the documentation.", Toast.LENGTH_LONG).show();
+                                        break;
 
-                                case "ERROR_CUSTOM_TOKEN_MISMATCH":
-                                    Toast.makeText(LoginActivity.this, "The custom token corresponds to a different audience.", Toast.LENGTH_LONG).show();
-                                    break;
+                                    case "ERROR_CUSTOM_TOKEN_MISMATCH":
+                                        Toast.makeText(LoginActivity.this, "The custom token corresponds to a different audience.", Toast.LENGTH_LONG).show();
+                                        break;
 
-                                case "ERROR_INVALID_CREDENTIAL":
-                                    Toast.makeText(LoginActivity.this, "The supplied auth credential is malformed or has expired.", Toast.LENGTH_LONG).show();
-                                    break;
+                                    case "ERROR_INVALID_CREDENTIAL":
+                                        Toast.makeText(LoginActivity.this, "The supplied auth credential is malformed or has expired.", Toast.LENGTH_LONG).show();
+                                        break;
 
-                                case "ERROR_INVALID_EMAIL":
-                                    Toast.makeText(LoginActivity.this, "The email address is badly formatted.", Toast.LENGTH_LONG).show();
-                                    etEmail.setError("The email address is badly formatted.");
-                                    etEmail.requestFocus();
-                                    break;
+                                    case "ERROR_INVALID_EMAIL":
+                                        Toast.makeText(LoginActivity.this, "The email address is badly formatted.", Toast.LENGTH_LONG).show();
+                                        etEmail.setError("The email address is badly formatted.");
+                                        etEmail.requestFocus();
+                                        break;
 
-                                case "ERROR_WRONG_PASSWORD":
-                                    Toast.makeText(LoginActivity.this, "The password is invalid or the user does not have a password.", Toast.LENGTH_LONG).show();
-                                    etPassword.setError("password is incorrect ");
-                                    etPassword.requestFocus();
-                                    etPassword.setText("");
-                                    break;
+                                    case "ERROR_WRONG_PASSWORD":
+                                        Toast.makeText(LoginActivity.this, "The password is invalid or the user does not have a password.", Toast.LENGTH_LONG).show();
+                                        etPassword.setError("password is incorrect ");
+                                        etPassword.requestFocus();
+                                        etPassword.setText("");
+                                        break;
 
-                                case "ERROR_USER_MISMATCH":
-                                    Toast.makeText(LoginActivity.this, "The supplied credentials do not correspond to the previously signed in user.", Toast.LENGTH_LONG).show();
-                                    break;
+                                    case "ERROR_USER_MISMATCH":
+                                        Toast.makeText(LoginActivity.this, "The supplied credentials do not correspond to the previously signed in user.", Toast.LENGTH_LONG).show();
+                                        break;
 
-                                case "ERROR_REQUIRES_RECENT_LOGIN":
-                                    Toast.makeText(LoginActivity.this, "This operation is sensitive and requires recent authentication. Log in again before retrying this request.", Toast.LENGTH_LONG).show();
-                                    break;
+                                    case "ERROR_REQUIRES_RECENT_LOGIN":
+                                        Toast.makeText(LoginActivity.this, "This operation is sensitive and requires recent authentication. Log in again before retrying this request.", Toast.LENGTH_LONG).show();
+                                        break;
 
-                                case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
-                                    Toast.makeText(LoginActivity.this, "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.", Toast.LENGTH_LONG).show();
-                                    break;
+                                    case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
+                                        Toast.makeText(LoginActivity.this, "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.", Toast.LENGTH_LONG).show();
+                                        break;
 
-                                case "ERROR_EMAIL_ALREADY_IN_USE":
-                                    Toast.makeText(LoginActivity.this, "The email address is already in use by another account.   ", Toast.LENGTH_LONG).show();
-                                    etEmail.setError("The email address is already in use by another account.");
-                                    etEmail.requestFocus();
-                                    break;
+                                    case "ERROR_EMAIL_ALREADY_IN_USE":
+                                        Toast.makeText(LoginActivity.this, "The email address is already in use by another account.   ", Toast.LENGTH_LONG).show();
+                                        etEmail.setError("The email address is already in use by another account.");
+                                        etEmail.requestFocus();
+                                        break;
 
-                                case "ERROR_CREDENTIAL_ALREADY_IN_USE":
-                                    Toast.makeText(LoginActivity.this, "This credential is already associated with a different user account.", Toast.LENGTH_LONG).show();
-                                    break;
+                                    case "ERROR_CREDENTIAL_ALREADY_IN_USE":
+                                        Toast.makeText(LoginActivity.this, "This credential is already associated with a different user account.", Toast.LENGTH_LONG).show();
+                                        break;
 
-                                case "ERROR_USER_DISABLED":
-                                    Toast.makeText(LoginActivity.this, "The user account has been disabled by an administrator.", Toast.LENGTH_LONG).show();
-                                    break;
+                                    case "ERROR_USER_DISABLED":
+                                        Toast.makeText(LoginActivity.this, "The user account has been disabled by an administrator.", Toast.LENGTH_LONG).show();
+                                        break;
 
-                                case "ERROR_USER_TOKEN_EXPIRED":
-                                    Toast.makeText(LoginActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
-                                    break;
+                                    case "ERROR_USER_TOKEN_EXPIRED":
+                                        Toast.makeText(LoginActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
+                                        break;
 
-                                case "ERROR_USER_NOT_FOUND":
-                                    Toast.makeText(LoginActivity.this, "There is no user record corresponding to this identifier. The user may have been deleted.", Toast.LENGTH_LONG).show();
-                                    break;
+                                    case "ERROR_USER_NOT_FOUND":
+                                        Toast.makeText(LoginActivity.this, "There is no user record corresponding to this identifier. The user may have been deleted.", Toast.LENGTH_LONG).show();
+                                        break;
 
-                                case "ERROR_INVALID_USER_TOKEN":
-                                    Toast.makeText(LoginActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
-                                    break;
+                                    case "ERROR_INVALID_USER_TOKEN":
+                                        Toast.makeText(LoginActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
+                                        break;
 
-                                case "ERROR_OPERATION_NOT_ALLOWED":
-                                    Toast.makeText(LoginActivity.this, "This operation is not allowed. You must enable this service in the console.", Toast.LENGTH_LONG).show();
-                                    break;
+                                    case "ERROR_OPERATION_NOT_ALLOWED":
+                                        Toast.makeText(LoginActivity.this, "This operation is not allowed. You must enable this service in the console.", Toast.LENGTH_LONG).show();
+                                        break;
 
-                                case "ERROR_WEAK_PASSWORD":
-                                    Toast.makeText(LoginActivity.this, "The given password is invalid.", Toast.LENGTH_LONG).show();
-                                    etPassword.setError("The password is invalid it must 6 characters at least");
-                                    etPassword.requestFocus();
-                                    break;
-                                default:
-                                    Toast.makeText(LoginActivity.this, "An internet connection is required to sign in. Please ensure you are connected to the internet and try again.", Toast.LENGTH_SHORT).show();
+                                    case "ERROR_WEAK_PASSWORD":
+                                        Toast.makeText(LoginActivity.this, "The given password is invalid.", Toast.LENGTH_LONG).show();
+                                        etPassword.setError("The password is invalid it must 6 characters at least");
+                                        etPassword.requestFocus();
+                                        break;
+                                }
+                            } else {
+                                Toast.makeText(LoginActivity.this, "An internet connection is required to sign in. Please ensure you are connected to the internet and try again.", Toast.LENGTH_SHORT).show();
                             }
-                            }
+
+                        }
+
                     }
                 });
     }
