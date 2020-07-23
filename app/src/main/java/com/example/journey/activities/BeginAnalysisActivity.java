45419e6f7e6c1ec64c2438246ea32f71a243356b
Matthew Rene Lacayo <mattlacayo@fb.com>
2020-07-23 15:48:30 -0700

    Setup on click listeners inside BeginAnalysisActivity

--- app/src/main/java/com/example/journey/activities/BeginAnalysisActivity.java
+++ app/src/main/java/com/example/journey/activities/BeginAnalysisActivity.java
@@ -3,18 +3,24 @@ package com.example.journey.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.journey.R;
 import com.example.journey.databinding.ActivityBeginAnalysisBinding;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
+import timber.log.Timber;
+
 public class BeginAnalysisActivity extends AppCompatActivity {
 
     private ActivityBeginAnalysisBinding binding;
 
     private TextView tvDateRange;
+    private EditText etTitle;
     private MaterialButton btSetDateFilter;
     private MaterialButton btStartAnalysis;
     private SwitchMaterial swKeyThemes;
@@ -31,6 +37,7 @@ public class BeginAnalysisActivity extends AppCompatActivity {
         binding = ActivityBeginAnalysisBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         bindElements();
+        setupElements();
     }
 
     private void bindElements() {
@@ -41,6 +48,61 @@ public class BeginAnalysisActivity extends AppCompatActivity {
         swFullMap = binding.swFullMap;
         swImportantEntries = binding.swImportantEntries;
         swMoodGraph = binding.swMoodGraph;
+        etTitle = binding.etTitle;
     }
+
+    private void setupElements() {
+        setupButtons();
+    }
+
+    private void setupButtons() {
+        btSetDateFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setDateFilterClicked();
+            }
+        });
+
+        btStartAnalysis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startAnalysisClicked();
+            }
+        });
+    }
+
+    private void startAnalysisClicked() {
+        Timber.d("Start Analysis Button Clicked.");
+        if (validateEntries()) {
+            //todo run analysis
+            boolean moodEnabled = swMoodGraph.isChecked();
+            boolean mapEnabled = swFullMap.isChecked();
+            boolean keyThemesEnabled = swKeyThemes.isChecked();
+            boolean keyEntriesEnabled = swImportantEntries.isChecked();
+            Timber.d("Key Theme Setting Enabled: " + keyThemesEnabled);
+            Timber.d("Map Setting Enabled: " + mapEnabled);
+            Timber.d("Key Entries Setting Enabled: " + keyEntriesEnabled);
+            Timber.d("Mood Setting Enabled: " + moodEnabled);
+            
+        } else {
+            Toast.makeText(this, "Please enter a title for your analysis first.", Toast.LENGTH_SHORT).show();
+            etTitle.setError("Title cannot be empty");
+            etTitle.requestFocus();
+        }
+    }
+
+    private void setDateFilterClicked() {
+        Timber.d("Set Date Filter Button Clicked.");
+    }
+
+    /**
+     * Method to ensure that all required entries are filled/valid.
+     * Checks to make sure that a title has been given, and returns true
+     * if the field is not empty.
+     */
+    private boolean validateEntries() {
+        return !etTitle.getText().toString().isEmpty();
+    }
+
 
 }
