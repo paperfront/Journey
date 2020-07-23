36ff30f6e17cf55dcb23bb014f99feca77607039
Matthew Rene Lacayo <mattlacayo@fb.com>
2020-07-23 12:26:39 -0700

    Setup view binding in begin analysis activity

--- app/src/main/java/com/example/journey/activities/BeginAnalysisActivity.java
+++ app/src/main/java/com/example/journey/activities/BeginAnalysisActivity.java
@@ -3,14 +3,44 @@ package com.example.journey.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.journey.R;
+import com.example.journey.databinding.ActivityBeginAnalysisBinding;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class BeginAnalysisActivity extends AppCompatActivity {
+
+    private ActivityBeginAnalysisBinding binding;
+
+    private TextView tvDateRange;
+    private MaterialButton btSetDateFilter;
+    private MaterialButton btStartAnalysis;
+    private SwitchMaterial swKeyThemes;
+    private SwitchMaterial swFullMap;
+    private SwitchMaterial swImportantEntries;
+    private SwitchMaterial swMoodGraph;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_begin_analysis);
+        binding = ActivityBeginAnalysisBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        bindElements();
     }
+
+    private void bindElements() {
+        tvDateRange = binding.tvDateRange;
+        btSetDateFilter = binding.btSetDateFilter;
+        btStartAnalysis = binding.btStartAnalysis;
+        swKeyThemes = binding.swKeyThemes;
+        swFullMap = binding.swFullMap;
+        swImportantEntries = binding.swImportantEntries;
+        swMoodGraph = binding.swMoodGraph;
+    }
+
 }
