da3cff5d60429b5ba78721a1e2dba394985d0893
Matthew Rene Lacayo <mattlacayo@fb.com>
2020-07-24 13:24:09 -0700

    Refined login activity error displays

--- app/src/main/java/com/example/journey/activities/LoginActivity.java
+++ app/src/main/java/com/example/journey/activities/LoginActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.opengl.ETC1;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +24,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthEmailException;
@@ -42,8 +46,10 @@ public class LoginActivity extends AppCompatActivity {
 
     private Button btLogin;
     private Button btSignup;
-    private EditText etEmail;
-    private EditText etPassword;
+    private TextInputEditText etEmail;
+    private TextInputEditText etPassword;
+    private TextInputLayout emailLayout;
+    private TextInputLayout passwordLayout;
 
     private ActivityLoginBinding binding;
     private FirebaseAuth mAuth;
@@ -67,10 +73,13 @@ public class LoginActivity extends AppCompatActivity {
         btSignup = binding.btSignUp;
         etEmail = binding.etEmail;
         etPassword = binding.etPassword;
+        emailLayout = binding.emailLayout;
+        passwordLayout = binding.passwordLayout;
     }
 
     private void setupElements() {
         setupButtons();
+        setupEditText();
     }
 
     private void setupButtons() {
@@ -85,6 +94,26 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 handleSignup();
+            }
+        });
+    }
+
+    private void setupEditText() {
+        etPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Timber.d("Text Changed");
+                passwordLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
     }
@@ -105,89 +134,7 @@ public class LoginActivity extends AppCompatActivity {
                             Timber.d("signInWithEmail:success");
                             goToMainActivity();
                         } else {
-                            // If sign in fails, display a message to the user.
-                            Exception e = task.getException();
-                            if (task.getException() instanceof FirebaseAuthException) {
-                                String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
-
-                                switch (errorCode) {
-
-                                    case "ERROR_INVALID_CUSTOM_TOKEN":
-                                        Toast.makeText(LoginActivity.this, "The custom token format is incorrect. Please check the documentation.", Toast.LENGTH_LONG).show();
-                                        break;
-
-                                    case "ERROR_CUSTOM_TOKEN_MISMATCH":
-                                        Toast.makeText(LoginActivity.this, "The custom token corresponds to a different audience.", Toast.LENGTH_LONG).show();
-                                        break;
-
-                                    case "ERROR_INVALID_CREDENTIAL":
-                                        Toast.makeText(LoginActivity.this, "The supplied auth credential is malformed or has expired.", Toast.LENGTH_LONG).show();
-                                        break;
-
-                                    case "ERROR_INVALID_EMAIL":
-                                        Toast.makeText(LoginActivity.this, "The email address is badly formatted.", Toast.LENGTH_LONG).show();
-                                        etEmail.setError("The email address is badly formatted.");
-                                        etEmail.requestFocus();
-                                        break;
-
-                                    case "ERROR_WRONG_PASSWORD":
-                                        Toast.makeText(LoginActivity.this, "The password is invalid or the user does not have a password.", Toast.LENGTH_LONG).show();
-                                        etPassword.setError("password is incorrect ");
-                                        etPassword.requestFocus();
-                                        etPassword.setText("");
-                                        break;
-
-                                    case "ERROR_USER_MISMATCH":
-                                        Toast.makeText(LoginActivity.this, "The supplied credentials do not correspond to the previously signed in user.", Toast.LENGTH_LONG).show();
-                                        break;
-
-                                    case "ERROR_REQUIRES_RECENT_LOGIN":
-                                        Toast.makeText(LoginActivity.this, "This operation is sensitive and requires recent authentication. Log in again before retrying this request.", Toast.LENGTH_LONG).show();
-                                        break;
-
-                                    case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
-                                        Toast.makeText(LoginActivity.this, "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.", Toast.LENGTH_LONG).show();
-                                        break;
-
-                                    case "ERROR_EMAIL_ALREADY_IN_USE":
-                                        Toast.makeText(LoginActivity.this, "The email address is already in use by another account.   ", Toast.LENGTH_LONG).show();
-                                        etEmail.setError("The email address is already in use by another account.");
-                                        etEmail.requestFocus();
-                                        break;
-
-                                    case "ERROR_CREDENTIAL_ALREADY_IN_USE":
-                                        Toast.makeText(LoginActivity.this, "This credential is already associated with a different user account.", Toast.LENGTH_LONG).show();
-                                        break;
-
-                                    case "ERROR_USER_DISABLED":
-                                        Toast.makeText(LoginActivity.this, "The user account has been disabled by an administrator.", Toast.LENGTH_LONG).show();
-                                        break;
-
-                                    case "ERROR_USER_TOKEN_EXPIRED":
-                                        Toast.makeText(LoginActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
-                                        break;
-
-                                    case "ERROR_USER_NOT_FOUND":
-                                        Toast.makeText(LoginActivity.this, "There is no user record corresponding to this identifier. The user may have been deleted.", Toast.LENGTH_LONG).show();
-                                        break;
-
-                                    case "ERROR_INVALID_USER_TOKEN":
-                                        Toast.makeText(LoginActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
-                                        break;
-
-                                    case "ERROR_OPERATION_NOT_ALLOWED":
-                                        Toast.makeText(LoginActivity.this, "This operation is not allowed. You must enable this service in the console.", Toast.LENGTH_LONG).show();
-                                        break;
-
-                                    case "ERROR_WEAK_PASSWORD":
-                                        Toast.makeText(LoginActivity.this, "The given password is invalid.", Toast.LENGTH_LONG).show();
-                                        etPassword.setError("The password is invalid it must 6 characters at least");
-                                        etPassword.requestFocus();
-                                        break;
-                                }
-                            } else {
-                                Toast.makeText(LoginActivity.this, "An internet connection is required to sign in. Please ensure you are connected to the internet and try again.", Toast.LENGTH_SHORT).show();
-                            }
+                            handleFirebaseError(task);
 
                         }
 
@@ -207,26 +154,115 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-
-                            // Sign in success, update UI with the signed-in user's information
                             Timber.d("createUserWithEmail:success");
                             FirebaseUser currentUser = mAuth.getCurrentUser();
                             saveUser(currentUser);
                             goToMainActivity();
                         } else {
-                            // If sign in fails, display a message to the user.
                             Timber.w(task.getException(), "createUserWithEmail:failure");
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            handleFirebaseError(task);
                         }
                     }
                 });
+    }
+
+    private void handleFirebaseError(Task task) {
+        // If sign in fails, display a message to the user.
+        Exception e = task.getException();
+        if (e instanceof FirebaseAuthException) {
+            String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+            handleErrorCode(errorCode);
+
+        } else {
+            showNetworkError();
+        }
     }
 
     private boolean inputsNonNull() {
         String email = etEmail.getText().toString();
         String password = etPassword.getText().toString();
         return !email.isEmpty() && !password.isEmpty();
+    }
+
+    private void handleErrorCode(String errorCode) {
+        switch (errorCode) {
+
+            case "ERROR_INVALID_CUSTOM_TOKEN":
+                Toast.makeText(LoginActivity.this, "The custom token format is incorrect. Please check the documentation.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_CUSTOM_TOKEN_MISMATCH":
+                Toast.makeText(LoginActivity.this, "The custom token corresponds to a different audience.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_INVALID_CREDENTIAL":
+                Toast.makeText(LoginActivity.this, "The supplied auth credential is malformed or has expired.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_INVALID_EMAIL":
+                Toast.makeText(LoginActivity.this, "The email address is badly formatted.", Toast.LENGTH_LONG).show();
+                emailLayout.setError("The email address is badly formatted.");
+                etEmail.requestFocus();
+                break;
+
+            case "ERROR_WRONG_PASSWORD":
+                Toast.makeText(LoginActivity.this, "The password is invalid or the user does not have a password.", Toast.LENGTH_LONG).show();
+                passwordLayout.setError("Password is incorrect ");
+                etPassword.requestFocus();
+                break;
+
+            case "ERROR_USER_MISMATCH":
+                Toast.makeText(LoginActivity.this, "The supplied credentials do not correspond to the previously signed in user.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_REQUIRES_RECENT_LOGIN":
+                Toast.makeText(LoginActivity.this, "This operation is sensitive and requires recent authentication. Log in again before retrying this request.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
+                Toast.makeText(LoginActivity.this, "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_EMAIL_ALREADY_IN_USE":
+                Toast.makeText(LoginActivity.this, "The email address is already in use by another account.   ", Toast.LENGTH_LONG).show();
+                emailLayout.setError("The email address is already in use by another account.");
+                etEmail.requestFocus();
+                break;
+
+            case "ERROR_CREDENTIAL_ALREADY_IN_USE":
+                Toast.makeText(LoginActivity.this, "This credential is already associated with a different user account.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_USER_DISABLED":
+                Toast.makeText(LoginActivity.this, "The user account has been disabled by an administrator.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_USER_TOKEN_EXPIRED":
+                Toast.makeText(LoginActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_USER_NOT_FOUND":
+                Toast.makeText(LoginActivity.this, "There is no user record corresponding to this identifier. The user may have been deleted.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_INVALID_USER_TOKEN":
+                Toast.makeText(LoginActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_OPERATION_NOT_ALLOWED":
+                Toast.makeText(LoginActivity.this, "This operation is not allowed. You must enable this service in the console.", Toast.LENGTH_LONG).show();
+                break;
+
+            case "ERROR_WEAK_PASSWORD":
+                Toast.makeText(LoginActivity.this, "The given password is invalid.", Toast.LENGTH_LONG).show();
+                passwordLayout.setError("The password is invalid it must 6 characters at least");
+                etPassword.requestFocus();
+                break;
+        }
+    }
+
+    private void showNetworkError() {
+        Toast.makeText(LoginActivity.this, "There was a problem connecting to the internet. Please try again later.", Toast.LENGTH_SHORT).show();
     }
 
     @Override
