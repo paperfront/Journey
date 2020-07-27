b5750a0f52f2a68a17bfcc2b5f472e398cce4a5c
Matthew Rene Lacayo <mattlacayo@fb.com>
2020-07-27 14:00:32 -0700

    Created basic word cloud functionality (all words appear in a vertical arrangement

--- app/src/main/java/com/example/journey/activities/AnalysisDetailActivity.java
+++ app/src/main/java/com/example/journey/activities/AnalysisDetailActivity.java
@@ -2,11 +2,15 @@ package com.example.journey.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import com.example.journey.R;
 import com.example.journey.databinding.ActivityAnalysisDetailBinding;
 import com.example.journey.models.Analysis;
+import com.example.journey.models.WordCloud;
 
 public class AnalysisDetailActivity extends AppCompatActivity {
 
@@ -14,6 +18,7 @@ public class AnalysisDetailActivity extends AppCompatActivity {
 
     public static final String KEY_ANALYSIS = "analysis";
     private Analysis analysis;
+    private FrameLayout flWordCloudHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +31,18 @@ public class AnalysisDetailActivity extends AppCompatActivity {
 
     private void bindElements() {
         analysis = getIntent().getParcelableExtra(KEY_ANALYSIS);
+        flWordCloudHolder = binding.flWordCloudHolder;
     }
 
     private void setupElements() {
+        if (analysis.getWordCounts() != null) {
+            setupWordCloud();
+        }
+    }
 
+    private void setupWordCloud() {
+        WordCloud wordCloud = new WordCloud(analysis.getWordCounts());
+        BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), wordCloud.createBitmap());
+        flWordCloudHolder.setBackground(bitmapDrawable);
     }
 }
